4d345fc928f24cd1640f64e587d6ed1a24b7da3e
ahsan007263-maker <ahsan.007263@gmail.com>
2026-02-14 15:42:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "I Love You Too, Mishu! ❤️";
-  gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmo3N3JueGZ3bm92Z3R4bm92Z3R4bm92Z3R4bm92Z3R4bm92Z3R4JmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCZjdD1z/iCVz2SSGuSDUFfIGAD/giphy.gif";
+  gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHpndWp3ZzRyeGZ3bm92Z3R4bm92Z3R4bm92Z3R4bm92Z3R4JmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCZjdD1z/K97ba8H05f4xJ972fO/giphy.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
